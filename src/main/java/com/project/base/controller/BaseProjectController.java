0f5b7d9cf238ba835585535e6edf5d269800c8af
lom <diskedge@yandex.ru>
2020-02-15 09:33:06 +0700

    thymeleaf bootstrap

--- src/main/java/com/project/base/controller/BaseProjectController.java
+++ src/main/java/com/project/base/controller/BaseProjectController.java
@@ -1,19 +1,28 @@
 package com.project.base.controller;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @Controller
 public class BaseProjectController {
 
+    @Value("injected message.")
+    String message;
+
     @RequestMapping("/")
-    String index(){
+    String index(Model model){
+//        String message = "Index page";
+        model.addAttribute("message",message);
         return "index";
     }
 
     @RequestMapping("/welcome")
-    String welcome(){
+    String welcome(Model model){
+        String welcomePage = "Welcome page";
+        model.addAttribute("welcomePage",welcomePage);
         return "welcome";
     }
 
