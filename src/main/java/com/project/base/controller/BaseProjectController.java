12cf010a9efec1666d76b7f6f8f3d721b687dc63
lom <diskedge@yandex.ru>
2020-02-15 09:04:01 +0700

    thymeleaf

--- src/main/java/com/project/base/controller/BaseProjectController.java
+++ src/main/java/com/project/base/controller/BaseProjectController.java
@@ -1,14 +1,20 @@
 package com.project.base.controller;
 
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@RestController
+@Controller
 public class BaseProjectController {
 
     @RequestMapping("/")
+    String index(){
+        return "index";
+    }
+
+    @RequestMapping("/welcome")
     String welcome(){
-        return "welcome page in tomcat";
+        return "welcome";
     }
 
 }
