cbbf3231ac84ead7c9d2a673a8b866077406f5f1
lom <solutionman@inbox.ru>
2020-02-19 21:46:33 +0700

    tables.sql

--- sql/tables.sql
+++ sql/tables.sql
@@ -51,5 +51,6 @@ INSERT INTO roles VALUES ( 4, 'ROLE_PERFORMER' );
 INSERT INTO users VALUES (1,'$2a$11$vwr8793lnCpvfD5T6/K16e0PXvTXGa0xE37Uez7WQJWoT4Wg5FN6K','admin','admin'); --12345678
 INSERT INTO user_roles values (1,2);
 
+INSERT INTO simplerecords values (1,'some record','short record');
 
 
