a127c245883983ac30d5271a6fd5e4db97561888
lom <solutionman@inbox.ru>
2020-04-20 20:09:47 +0700

    serverSide testing

--- src/main/resources/static/js/serverSideTable.js
+++ src/main/resources/static/js/serverSideTable.js
@@ -2,6 +2,8 @@ $(document).ready( function () {
     var table = $('#testRecords').DataTable({
         "sAjaxSource": "/base/testRecords",
         "sAjaxDataProp": "",
+        "processing": false,
+        "serverSide": false,
         "order": [[ 0, "asc" ]],
         "aoColumns": [
             { "mData": "username"},
