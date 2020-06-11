1161d101a4ceebef0d5249ecd647f6c8f78e25aa
lom <lom@ssvet.info>
2020-06-11 08:14:18 +0700

    for stackOverflow question 62296439

--- src/main/resources/static/js/serverSideTable.js
+++ src/main/resources/static/js/serverSideTable.js
@@ -1,10 +1,14 @@
 $(document).ready( function () {
-    var table = $('#testRecords').DataTable({
-        ajax: '/base/testRecords',
-        sAjaxDataProp: 'data',
-        processing: true,
-        serverSide: true,
-        // "order": [[ 0, "asc" ]],
+    $('#testRecords').DataTable({
+        ajax: {
+            url: '/base/testRecords',
+            contentType: 'application/json',
+            type: 'GET',
+            data: function (d) {
+                // console.log(d);
+                return JSON.stringify( d );
+            }
+        },
         columns: [
             { data: "username", title : 'username'},
             { data: "firstname", title: 'firstname' },
